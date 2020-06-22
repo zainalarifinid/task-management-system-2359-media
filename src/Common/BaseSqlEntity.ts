1ef852bd7dfa93247f7d61377c39ef13279084e3
Zainal Arifin <arifin.1602@gmail.com>
2020-06-23 02:28:42 +0700

    feat(InserTask): Insert Task into DB, update test reuse mock for repository and entities

--- src/Common/BaseSqlEntity.ts
+++ src/Common/BaseSqlEntity.ts
@@ -1,27 +1,30 @@
 import { ObjectLiteral, CreateDateColumn } from "typeorm";
+type Nullable<T> = T | null;
 
 export class BaseSqlEntity implements ObjectLiteral{
   
   @CreateDateColumn({
     name: 'created_at',
-    type: process.env.NODE_ENV === 'test' ? 'datetime' : 'timestamptz',
+    type: process.env.NODE_ENV === 'test' ? 'datetime' : 'timestamp',
     precision: 0,
   })
-  createdAt: Date = null;
+  createdAt: Date = new Date();
   
   @CreateDateColumn({
     name: 'update_at',
-    type: process.env.NODE_ENV === 'test' ? 'datetime' : 'timestamptz',
+    type: process.env.NODE_ENV === 'test' ? 'datetime' : 'timestamp',
     precision: 0,
   })
-  updateAt: Date = null;
+  updateAt: Date = new Date();
 
   @CreateDateColumn({
     name: 'delete_at',
-    type: process.env.NODE_ENV === 'test' ? 'datetime' : 'timestamptz',
+    type: process.env.NODE_ENV === 'test' ? 'datetime' : 'timestamp',
     precision: 0,
+    nullable: true,
+    default: null,
   })
-  deletedAt: Date = null;
+  deletedAt: Nullable<Date> = null;
 
   public delete(): void {
     this.deletedAt = new Date();
--- src/Task/services/__test__/TaskService.spec.ts
+++ src/Task/services/__test__/TaskService.spec.ts
@@ -1,5 +1,6 @@
 import { TaskService } from '../TaskService'
 import { TaskRepository } from '../../repositories/TaskRepository';
+import { Task } from '../../entities/Task';
 
 jest.mock('../../repositories/TaskRepository');
 
@@ -13,14 +14,39 @@ describe('Process The Task', () => {
     taskService = new TaskService(taskRepositoryMock);
   })
 
-  it('should return error if given empty command', () => {
-    expect(taskService.processCommand(command)).toBeInstanceOf(Error);
-    expect(taskService.processCommand(command)).toMatchObject({message: 'Please insert correct command', code: 400});
+  it('should return error if given empty command', async () => {
+    try {
+      await taskService.processCommand(command);
+    }catch (err) {
+      expect(err).toBeInstanceOf(Error);
+      expect(err).toMatchObject({message: 'Please insert correct command', code: 400});
+    }
   });
 
-  it('should return correct complete field when given correct complete command', () => {
+  it('should return correct complete field when given correct complete command', async() => {
     command = 'Oct 9, 2017 @ 9am, Community Centre, Swimming';
-    expect(taskService.processCommand(command)).toMatchObject({
+    const exampleDataTask = new Task();
+    exampleDataTask.activity = 'Swimming';
+    exampleDataTask.startTime = '2017-10-09T09:00:00+00:00' as any;
+    exampleDataTask.endTime = '2017-10-09T10:00:00+00:00' as any;
+    exampleDataTask.place = 'Community Centre';
+    exampleDataTask.originalCommand = command;
+    exampleDataTask.idParent = 0;
+    exampleDataTask.createdAt = expect.any(Date);
+    exampleDataTask.updateAt = expect.any(Date);
+
+    const exampleResult = {
+      activity: 'Swimming',
+      startTime: '2017-10-09T09:00:00+00:00',
+      endTime: '2017-10-09T10:00:00+00:00',
+      place: 'Community Centre',
+      originalCommand: command,
+      idParent: 0
+    };
+    taskRepositoryMock.save.mockResolvedValueOnce( exampleResult as any );
+    const resultTaskManagement = await taskService.processCommand(command);
+    expect(taskRepositoryMock.save).toHaveBeenCalledWith( exampleDataTask );
+    expect(resultTaskManagement).toMatchObject({
       activity: 'Swimming',
       startTime: '2017-10-09T09:00:00+00:00',
       endTime: '2017-10-09T10:00:00+00:00',
@@ -29,13 +55,56 @@ describe('Process The Task', () => {
     });
   });
 
-  it('should return correct field when given activity with child', () => {
+  it('should return correct field when given activity with child', async() => {
     command = 'Oct 11, 2017 @ 2pm - 6pm, University, Final Exam';
     const child = [
       '2pm - 4pm, Mathematic Exam',
       '4pm - 6pm, Physic Exam'
     ];
-    expect(taskService.processCommand(command, child)).toMatchObject({
+
+    const exampleDataTaskParent = new Task();
+    exampleDataTaskParent.activity = 'Final Exam';
+    exampleDataTaskParent.startTime = '2017-10-11T14:00:00+00:00' as any;
+    exampleDataTaskParent.endTime = '2017-10-11T18:00:00+00:00' as any;
+    exampleDataTaskParent.place = 'University';
+    exampleDataTaskParent.originalCommand = command;
+    exampleDataTaskParent.idParent = 0;
+    exampleDataTaskParent.createdAt = expect.any(Date);
+    exampleDataTaskParent.updateAt = expect.any(Date);
+
+    const exampleResultParent = {
+      id: 1000,
+      activity: 'Final Exam',
+      startTime: '2017-10-11T14:00:00+00:00',
+      endTime: '2017-10-11T18:00:00+00:00',
+      place: 'University',
+      originalCommand: command,
+      idParent: 0
+    };
+
+    const exampleResultChildFirst = {
+      activity: 'Mathematic Exam',
+      startTime: '2017-10-11T14:00:00+00:00',
+      endTime: '2017-10-11T16:00:00+00:00',
+      place: null,
+      originalCommand: '2pm - 4pm, Mathematic Exam'
+    };
+
+    const exampleResultChildSecond = {
+      activity: 'Physic Exam',
+      startTime: '2017-10-11T16:00:00+00:00',
+      endTime: '2017-10-11T18:00:00+00:00',
+      place: null,
+      originalCommand: '4pm - 6pm, Physic Exam'
+    }
+
+    taskRepositoryMock.save.mockResolvedValueOnce( exampleResultParent as any );
+    taskRepositoryMock.save.mockResolvedValueOnce( exampleResultChildFirst as any );
+    taskRepositoryMock.save.mockResolvedValueOnce( exampleResultChildSecond as any );
+    const resultTaskManagement = await taskService.processCommand(command, child);
+    expect(taskRepositoryMock.save).toHaveBeenCalledWith( exampleDataTaskParent );
+
+    expect(resultTaskManagement).toMatchObject({
       activity: 'Final Exam',
       startTime: '2017-10-11T14:00:00+00:00',
       endTime: '2017-10-11T18:00:00+00:00',
@@ -60,14 +129,36 @@ describe('Process The Task', () => {
     })
   });
 
-  it('should return correct field when given simple activity', () => {
-    command = 'Nov 15, 2017, Harry’s birthday';
-    expect(taskService.processCommand(command)).toMatchObject({
-      activity: 'Harry’s birthday',
+  it('should return correct field when given simple activity', async() => {
+    command = "Nov 15, 2017, Harry’s birthday";
+
+    const exampleDataTask = new Task();
+    exampleDataTask.activity = 'Harry\’s birthday';
+    exampleDataTask.startTime = '2017-11-15T00:00:00+00:00' as any;
+    exampleDataTask.endTime = '2017-11-16T00:00:00+00:00' as any;
+    exampleDataTask.place = null;
+    exampleDataTask.originalCommand = command;
+    exampleDataTask.idParent = 0;
+    exampleDataTask.createdAt = expect.any(Date);
+    exampleDataTask.updateAt = expect.any(Date);
+
+    const exampleResult = {
+      activity: 'Harry\’s birthday',
+      startTime: '2017-11-15T00:00:00+00:00',
+      endTime: '2017-11-16T00:00:00+00:00',
+      place: null,
+      originalCommand: command,
+      idParent: 0
+    };
+    taskRepositoryMock.save.mockResolvedValueOnce( exampleResult as any );
+    const resultTaskManagement = await taskService.processCommand(command);
+    expect(taskRepositoryMock.save).toHaveBeenCalledWith( exampleDataTask );
+    expect(resultTaskManagement).toMatchObject({
+      activity: 'Harry\’s birthday',
       startTime: '2017-11-15T00:00:00+00:00',
       endTime: '2017-11-16T00:00:00+00:00',
       place: null,
       originalCommand: command
-    })
+    });
   });
 });
